f76f62bce2a9d7445fad804676959fe74c5b24c5
Gray Pearson <122818113+GrayPearson@users.noreply.github.com>
2023-03-27 12:35:23 -0400

    Imported Example code

--- create_relationships.py
+++ create_relationships.py
@@ -7,6 +7,7 @@ Usage:
  python create_relationships.py
 """
 import os
+import sqlite3
 
 # Determine the path of the database
 db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'social_network.db')
@@ -18,7 +19,20 @@ def main():
 def create_relationships_table():
     """Creates the relationships table in the DB"""
     # TODO: Function body
-    return
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    create_relationships_tbl_query =
+        id INTEGER PRIMARY KEY,
+        person1_id INTEGER NOT NULL,
+        person2_id INTEGER NOT NULL,
+        type TEXT NOT NULL,
+        start_date DATE NOT NULL,
+        FOREIGN KEY (person1_id) REFERENCES people (id),
+        FOREIGN KEY (person2_id) REFERENCES people (id)
+cur.execute(create_relationships_tbl_query)
+con.commit()
+con.close()
+return
 
 def populate_relationships_table():
     """Adds 100 random relationships to the DB"""
